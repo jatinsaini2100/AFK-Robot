af2f8ff4dec06b0bb41a6591540db22f21b9338a
jatinsaini2100 <128668150+jatinsaini2100@users.noreply.github.com>
2024-02-25 01:19:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", 22980504))
 API_HASH = getenv("API_HASH", b85ecdd508ae39f4e64a08e9a4f9c48d)
 
-BOT_TOKEN = getenv("BOT_TOKEN", 6686420045:AAH4xpOg3cfDtRK4TP7usMe7Mzl3rmej8dw)
+BOT_TOKEN = getenv("BOT_TOKEN", "6686420045:AAH4xpOg3cfDtRK4TP7usMe7Mzl3rmej8dw")
 LOG_ID = int(getenv("LOG_ID", ""))
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", mongodb+srv://tonyxbot:tonyxbot@cluster0.bojrxzg.mongodb.net/?retryWrites=true&w=majority)
