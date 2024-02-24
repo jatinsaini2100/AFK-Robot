5a627ba98cc866375931eeae7cd702143dde9b8d
jatinsaini2100 <128668150+jatinsaini2100@users.noreply.github.com>
2024-02-25 01:56:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,4 +19,4 @@ API_HASH = getenv("API_HASH", "5a8e5c861695595f38ec59b6e0a0f6cc")
 BOT_TOKEN = getenv("BOT_TOKEN", "6686420045:AAH4xpOg3cfDtRK4TP7usMe7Mzl3rmej8dw")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://tonyxbot:tonyxbot@cluster0.bojrxzg.mongodb.net/?retryWrites=true&w=majority")
-SUDO_USER = list(map(int, getenv("SUDO_USER", 5366891026).split()))
+SUDO_USER = list(map(int, getenv("SUDO_USER", "5366891026").split()))
