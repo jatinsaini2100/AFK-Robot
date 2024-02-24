b92d87acfcd57bdd427707374ca0d7e2031a571b
jatinsaini2100 <128668150+jatinsaini2100@users.noreply.github.com>
2024-02-25 02:49:18 +0530

    Update afk.py

--- AfkRobot/modules/afk.py
+++ AfkRobot/modules/afk.py
@@ -172,7 +172,8 @@ async def active_afk(_, message: Message):
             "reason": None,
         }
 
-     await add_afk(user_id, details)
+     async def active_afk(_, message: Message):
+    await add_afk(user_id, details)
     send = await message.reply_text(
         f"{message.from_user.first_name} is now afk!"
     )
