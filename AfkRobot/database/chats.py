763f48f72a196441264dd309f3e37aca83ab4744
jatinsaini2100 <128668150+jatinsaini2100@users.noreply.github.com>
2024-02-25 16:33:11 +0530

    Update chats.py

--- AfkRobot/database/chats.py
+++ AfkRobot/database/chats.py
@@ -7,13 +7,10 @@ chatsdb = db.chatsdb
 
 
 async def get_served_chats() -> list:
-    chats = chatsdb.find({"chat_id": {"$lt": 0}})
+    chats = await chatsdb.find({}).to_list(length=1000000000)
     if not chats:
         return []
-    chats_list = []
-    for chat in await chats.to_list(length=1000000000):
-        chats_list.append(chat)
-    return chats_list
+    return chats
 
 
 async def is_served_chat(chat_id: int) -> bool:
