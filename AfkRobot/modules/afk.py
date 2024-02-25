839bc80dd49f20dfa21351d2e50aefd7784eb7d0
jatinsaini2100 <128668150+jatinsaini2100@users.noreply.github.com>
2024-02-25 14:29:45 +0530

    Update afk.py

--- AfkRobot/modules/afk.py
+++ AfkRobot/modules/afk.py
@@ -174,7 +174,8 @@ async def active_afk(_, message: Message):
 
      
     await add_afk(user_id, details)
-    send = await message.reply_text(
-        f"{message.from_user.first_name} is now afk!"
-    )
-    await put_cleanmode(message.chat.id, send.message_id)
+send = await message.reply_text(
+    f"{message.from_user.first_name} is now afk!"
+)
+print(type(send))  # This will print the type of send
+await put_cleanmode(message.chat.id, send.message_id)
